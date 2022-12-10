87e126b4b2c40746f21acf6a9c8dc294990967c3
Joe King <j.king92@gmail.com>
2022-12-10 15:39:52 -0700

    Add resident index page listing all residents with their attributes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+resident1 = Resident.create!(name: "Jessica Rabbit", age: 68, occupation: "Singer")
+resident2 = Resident.create!(name: "Seth Rogan", age: 43, occupation: "Comedian")
+resident3 = Resident.create!(name: "Miles Morales", age: 18, occupation: "Student/Spiderman")
---
+++ spec/features/residents/index_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'resident index page' do
+  it 'shows a list of residents with their name age and occupation' do
+    resident1 = Resident.create!(name: "Jessica Rabbit", age: 68, occupation: "Singer")
+    resident2 = Resident.create!(name: "Seth Rogan", age: 43, occupation: "Comedian")
+    resident3 = Resident.create!(name: "Miles Morales", age: 18, occupation: "Student/Spiderman")
+
+    visit '/residents'
+    
+    expect(page).to have_content("Name: Jessica Rabbit")
+    expect(page).to have_content("Name: Seth Rogan")
+    expect(page).to have_content("Age: 43")
+    expect(page).to have_content("Age: 18")
+    expect(page).to have_content("Occupation: Student/Spiderman")
+    expect(page).to have_content("Occupation: Singer")   
+  end
+end
