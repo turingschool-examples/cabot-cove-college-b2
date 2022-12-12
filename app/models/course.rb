b3ec8d0d84947e234262ff070e9e68c956100fd5
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-12-11 23:08:03 -0600

    add courses index functionality

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,8 @@ class Course < ApplicationRecord
   validates :name, presence: true
   has_many :resident_courses
   has_many :residents, through: :resident_courses
+
+  def resident_count
+    self.residents.count
+  end
 end
