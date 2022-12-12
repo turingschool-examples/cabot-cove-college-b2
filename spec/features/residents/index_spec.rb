539462ddc7d3011b403eafe745130ee8c01f2a45
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-12-11 23:37:32 -0600

    add residents average age functionality on index page

--- spec/features/residents/index_spec.rb
+++ spec/features/residents/index_spec.rb
@@ -10,12 +10,22 @@ require 'rails_helper'
 # (e.g. "Name: Jessica Fletcher, Age: 65, Occupation: Mystery writer", 
 #       "Name: Dr. Seth Hazlitt, Age: 70, Occupation: Town Doctor")
 
+
+# User Story 4 of 4
+
+# As a visitor,
+# When I visit '/residents'
+# I see the average age of all residents.
+
+# (e.g. "Average Age: 72.5")
+
 RSpec.describe 'residents index' do
   describe 'As a visitor' do
     describe "When I visit '/residents'" do
       before :each do
         @res_1 = Resident.create!(name: 'Jessica Fletcher', age: 65, occupation: 'Mystery writer')
         @res_2 = Resident.create!(name: 'Dr. Seth Hazlitt', age: 70, occupation: 'Town Doctor')
+        @res_3 = Resident.create!(name: 'Jackson Pollock', age: 22, occupation: 'Painter')
         visit '/residents'
       end
 
@@ -26,6 +36,13 @@ RSpec.describe 'residents index' do
         expect(page).to have_content("Name: #{@res_2.name}")
         expect(page).to have_content("Age: #{@res_2.age}")
         expect(page).to have_content("Occupation: #{@res_2.occupation}")
+        expect(page).to have_content("Name: #{@res_3.name}")
+        expect(page).to have_content("Age: #{@res_3.age}")
+        expect(page).to have_content("Occupation: #{@res_3.occupation}")
+      end
+
+      it 'I see the average age of all residents' do
+        expect(page).to have_content('Average Age: 52.33')
       end
     end
   end
--- spec/models/resident_spec.rb
+++ spec/models/resident_spec.rb
@@ -12,5 +12,18 @@ RSpec.describe Resident, type: :model do
     it {should have_many :resident_courses}
     it {should have_many(:courses).through(:resident_courses)}
   end
+  
+  describe 'class methods' do
+    describe '#resident_average_age' do
+      before :each do
+        @res_1 = Resident.create!(name: 'Jessica Fletcher', age: 65, occupation: 'Mystery writer')
+        @res_2 = Resident.create!(name: 'Dr. Seth Hazlitt', age: 70, occupation: 'Town Doctor')
+        @res_3 = Resident.create!(name: 'Jackson Pollock', age: 22, occupation: 'Painter')
+      end
+      it 'returns the average age of all residents' do
+        expect(Resident.resident_average_age.round(2)).to eq(52.33)
+      end
+    end
+  end
 
 end
