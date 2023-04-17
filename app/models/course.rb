f9afaf8e01c299b0485b6412a480b00576b19b5e
James White <whitejaad@gmail.com>
2023-04-17 12:03:43 -0700

    fixed models validations and relationships

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,5 @@
 class Course < ApplicationRecord
-
+  has_many :resident_courses
+  has_many :residents, through: :resident_courses
+  validates_presence_of :name
 end
--- app/models/resident.rb
+++ app/models/resident.rb
@@ -1,3 +1,5 @@
 class Resident < ApplicationRecord
-
+  has_many :resident_courses
+  has_many :courses, through: :resident_courses
+  validates_presence_of :name, :occupation, :age
 end
