d0be4ca731b84f684737b9bc8d2626d2ffcf6a15
James White <whitejaad@gmail.com>
2023-04-18 13:40:08 -0700

    first residents index test pass

---
+++ spec/features/index_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+# User Story 1 of 4
+
+# As a visitor,
+# When I visit '/residents',
+# I see a list of residents with the following information:
+# - Name
+# - Age
+# - Occupation
+# (e.g. "Name: Jessica Fletcher, Age: 65, Occupation: Mystery writer", 
+#       "Name: Dr. Seth Hazlitt, Age: 70, Occupation: Town Doctor")
+RSpec.describe 'resident index' do
+  describe 'as a visitor' do 
+    before :each do
+     @res_1 = Resident.create!(name: 'Jessica Fletcher', age: 65, occupation: 'Mystery writer')
+     @res_2 = Resident.create!(name: 'Dr. Seth Hazlitt', age: 70, occupation: 'Town Doctor')
+    end
+     describe ' when I visit /residents' do 
+      it ' I see a list of residents with their name, age, and occupation' do 
+
+        visit '/residents'
+        
+        expect(page).to have_content("Residents Index")
+      end
+     end
+   end
+end
+
+
+
