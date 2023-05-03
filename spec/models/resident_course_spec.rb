878fd67623b51c17b3ff7b51cf1c4bf17a38c2dc
Chris Simmons <chris@turing.edu>
2023-05-03 10:47:52 -0400

    Fixes tests

--- spec/models/resident_course_spec.rb
+++ spec/models/resident_course_spec.rb
@@ -1,8 +1,8 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe ResidentCourse, type: :model do
 
-  describe 'relationships' do
+  describe "relationships" do
     it {should belong_to :resident}
     it {should belong_to :course}
   end
--- spec/models/resident_spec.rb
+++ spec/models/resident_spec.rb
@@ -1,14 +1,14 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Resident, type: :model do
 
-  describe 'validations' do
+  describe "validations" do
     it {should validate_presence_of :name}
     it {should validate_presence_of :age}
     it {should validate_presence_of :occupation}
   end
 
-  describe 'relationships' do
+  describe "relationships" do
     it {should have_many :resident_courses}
     it {should have_many(:courses).through(:resident_courses)}
   end
